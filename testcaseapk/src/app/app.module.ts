34d2858b273881598b77ea31280fa0c6f87daf80
Snobin <abrahamsnobin2001@gmail.com>
2023-12-08 11:19:08 +0530

    ng g c

--- testcaseapk/src/app/app.module.ts
+++ testcaseapk/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
