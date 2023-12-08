73a083fca1c048a6350a7b1cdbf9765dc6ed26ac
Mijohn T J <mijohn2000@gmail.com>
2023-12-08 12:02:57 +0530

    Revert "updated"
    This reverts commit acea112ecf0f25d2865c9c107f3cc025521cae31.

--- testcaseapk/src/app/app-routing.module.ts
+++ testcaseapk/src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
 
 
-const routes: Routes = [
-  {path: 'landing', component: LandingComponent},
-  {path: '',redirectTo:'landing',pathMatch:'full'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
