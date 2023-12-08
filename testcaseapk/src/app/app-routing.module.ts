acea112ecf0f25d2865c9c107f3cc025521cae31
Mijohn T J <mijohn2000@gmail.com>
2023-12-08 11:50:31 +0530

    updated

--- testcaseapk/src/app/app-routing.module.ts
+++ testcaseapk/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landing', component: LandingComponent},
+  {path: '',redirectTo:'landing',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
