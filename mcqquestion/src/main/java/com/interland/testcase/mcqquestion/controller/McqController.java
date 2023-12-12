3c174e68356126cf5e81969ee84ca027b211a759
Parvathy7278 <parvathybabubabut@gmail.com>
2023-12-12 17:00:10 +0530

    mcqmicroservices

---
+++ mcqquestion/src/main/java/com/interland/testcase/mcqquestion/controller/McqController.java
@@ -0,0 +1,65 @@
+package com.interland.testcase.mcqquestion.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.interland.testcase.mcqquestion.dto.Dto;
+import com.interland.testcase.mcqquestion.dto.ServiceResponse;
+import com.interland.testcase.mcqquestion.exception.RecordNotFoundException;
+import com.interland.testcase.mcqquestion.service.McqService;
+
+import jakarta.validation.Valid;
+
+
+
+@RestController   
+@RequestMapping("/MCQ")
+public class McqController {
+	
+	@Autowired
+	McqService mcqservice;
+	
+	
+	@GetMapping("/Get/{questionNo}/{questionId}")
+	public ResponseEntity<Dto> getUserById(@PathVariable("questionNo") String questionNo,@PathVariable("questionId")String questionId ) {
+			return new ResponseEntity<>(mcqservice.getById(questionNo,questionId), new HttpHeaders(), HttpStatus.ACCEPTED);
+		} 
+	
+	
+	@PostMapping("/questions")
+	public ResponseEntity<ServiceResponse> create( @RequestBody Dto dto){
+		System.out.println("REACHED");
+	return new ResponseEntity<>(mcqservice.create(dto), new HttpHeaders(), HttpStatus.OK);
+	}
+	
+	
+	@PutMapping("/putquestions")
+	public ResponseEntity<ServiceResponse> update(@Valid @RequestBody Dto dto)
+	{
+		return new ResponseEntity<>(mcqservice.updateQuestion(dto), new HttpHeaders(), HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	@DeleteMapping("/deleteQuestion/{questionNo}/{questionId}")
+	public ResponseEntity<ServiceResponse> deleteCardUserById(@PathVariable String questionNo , @PathVariable String questionId) throws RecordNotFoundException
+	 {
+		return new ResponseEntity<>(mcqservice.delete(questionNo, questionId), new HttpHeaders(),
+				HttpStatus.OK);
+	}
+
+
+}
---
+++ mcqquestion/src/main/java/com/interland/testcase/mcqquestion/util/Constants.java
@@ -0,0 +1,26 @@
+package com.interland.testcase.mcqquestion.util;
+
+
+public class Constants {
+	
+	public final static String QUESTIONNO="questionno";
+	public final static String QUESTIONID="questionid";
+	public final static String QUESTION="question";
+	public final static String OPTIONA="optiona";
+	public final static String OPTIONB="optionb";
+	public final static String OPTIONC="optionc";
+	public final static String OPTIOND="optiond";
+	public final static String ANSWERS="answers";
+	public final static String SCORE="score";
+	public final static String SUCCESS = "SUCCESS";
+	public final static String VERIFYDELETED = "verifydeleted";
+
+
+
+public static class MESSAGE_STATUS{
+	public final static String SUCCESS = "SUCCESS";
+	public final static String FAILED = "FAILED";
+	public final static String DELETED = "DELETED";
+	
+}
+}
