a3346e9131c732414faa9ef1176edb2f027b5eca
Parvathy7278 <parvathybabubabut@gmail.com>
2023-12-14 16:24:09 +0530

    updated

--- mcqquestion/src/main/java/com/interland/testcase/mcqquestion/repository/McqRepository.java
+++ mcqquestion/src/main/java/com/interland/testcase/mcqquestion/repository/McqRepository.java
@@ -2,6 +2,8 @@ package com.interland.testcase.mcqquestion.repository;
 
 import java.util.ArrayList;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,6 +15,7 @@ import com.interland.testcase.mcqquestion.entity.McqEntity;
 public interface McqRepository extends JpaRepository<McqEntity, McqEmbedded>{
 
 	ArrayList findAll(Specification<McqEntity> spec);
+	Page<McqEntity> findAll(Specification<McqEntity> spec, Pageable pageable);
 
 }
 
