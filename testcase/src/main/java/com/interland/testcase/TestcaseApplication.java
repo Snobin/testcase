d9fb42bfa9cd6c7723c5e5766ea7afd8963570c3
Snobin <abrahamsnobin2001@gmail.com>
2023-12-20 11:38:12 +0530

    resolving

--- testcase/src/main/java/com/interland/testcase/TestcaseApplication.java
+++ testcase/src/main/java/com/interland/testcase/TestcaseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@
+
 public class TestcaseApplication {
 
 	public static void main(String[] args) {
