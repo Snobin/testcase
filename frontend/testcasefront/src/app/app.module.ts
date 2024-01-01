374be23a3cbcfa927f2fac749152d7166ec8f08b
Mijohn T J <mijohn2000@gmail.com>
2024-01-01 11:25:40 +0530

    updated

--- frontend/testcasefront/src/app/app.module.ts
+++ frontend/testcasefront/src/app/app.module.ts
@@ -49,9 +49,6 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule,
     MatButtonModule,MatCardModule,MatToolbarModule,MatIconModule,MatListModule,MatInputModule,MatFormFieldModule,MatSnackBarModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,MatCardModule,MatToolbarModule,MatIconModule,MatListModule,MatInputModule,MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- testcaseapk/src/app/login/login.component.ts
+++ testcaseapk/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from '../app.component';
 import Swal from 'sweetalert2';
+import { LoginService } from '../service/login.service';
 
 declare var $:any;
 @Component({
@@ -15,26 +16,89 @@ username: any;
 password: any;
 isAdmin: any=false;
 
-  constructor(private router: Router, public obj:AppComponent) { }
+loginData = {
+  email: '',
+  password: ''
+}
+hidePassword = true;
+
+  constructor(private login:LoginService, private router: Router, public obj:AppComponent) { }
 
   ngOnInit(): void {
   }
-
-  login(){
-    this.obj.isLoggedIn=true;
-    if (this.isAdmin==false) {
-      this.router.navigate(['./exam/instructions']);
-    } else {
-      this.router.navigate(['./admin']);
-    }
-    Swal.fire({
-      text: 'You are ready to go!',
-      showConfirmButton: false,
-      timer: 600,
-      position: 'top'
-    })
-  }
+  
   backToLogin(){
-    this.router.navigate(['./login']);
+    this.router.navigate([`./login`]);
   }
+
+  formSubmit() {
+    // Add the logic to validate and submit the data here
+    if (this.loginData.email.trim()=='' || this.loginData.email.trim()==null) {
+      Swal.fire({
+        text: "Email is required !",
+        showConfirmButton: false,
+        timer: 600,
+        position: 'top'
+      });
+      return;
+    }
+    if (this.loginData.password.trim()=='' || this.loginData.password.trim()==null) {
+      Swal.fire({
+        text: "Password is required !",
+        showConfirmButton: false,
+        timer: 600,
+        position: 'top'
+      });
+      return;
+    }
+
+    //request to server to generate token
+    this.login.generateToken(this.loginData).subscribe(
+      (data:any) => {
+        if (data.token && data.role == 'USER') {
+          this.login.loginUser(data.token);
+          this.login.setUser(data.username);
+          this.login.setUserRole(data.role);
+          this.router.navigate([`./exam/instructions`]);
+          Swal.fire({
+            text: "Welcome to Interland",
+            showConfirmButton: false,
+            timer: 600,
+            position: 'top'
+          });
+        } else if (data.token && data.role == 'ADMIN') {
+          this.login.loginUser(data.token);
+          this.login.setUser(data.username);
+          this.login.setUserRole(data.role);
+          this.router.navigate([`./admin`]);
+          Swal.fire({
+            text: "Welcome to admin dasboard",
+            showConfirmButton: false,
+            timer: 600,
+            position: 'top'
+          });
+        } else {
+          Swal.fire({
+            text: "An unexpected error occured !",
+            showConfirmButton: false,
+            timer: 600,
+            position: 'top'
+          });
+        }
+      },
+      (error) => {
+        Swal.fire({
+          text: error.error+' !',
+          showConfirmButton: false,
+          timer: 600,
+          position: 'top'
+        });
+      }
+    )
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
 }
