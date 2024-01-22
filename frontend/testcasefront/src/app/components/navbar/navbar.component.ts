4cc48b683a7d0c0ec8e67d74ec2fc40106c9effe
Mijohn T J <mijohn2000@gmail.com>
2024-01-22 18:32:50 +0530

    updated timer

--- frontend/testcasefront/src/app/components/navbar/navbar.component.ts
+++ frontend/testcasefront/src/app/components/navbar/navbar.component.ts
@@ -11,6 +11,8 @@ export class NavbarComponent implements OnInit {
 
   isLoggedIn=false;
   user=null;
+  countdownMinutes: number = 10;
+  countdownSeconds: number = 0;
 
   constructor(public login:LoginService, private router: Router) { }
 
@@ -33,6 +35,22 @@ export class NavbarComponent implements OnInit {
     this.isLoggedIn=this.login.isloggedin();
     this.user=this.login.getUser();
     this.router.navigate([`login`]);
+  }
+
+  startCountdown(): void {
+    const countdownInterval = setInterval(() => {
+      if (this.countdownSeconds === 0) {
+        if (this.countdownMinutes === 0) {
+          clearInterval(countdownInterval);
+          // Handle what happens when the countdown reaches 0
+        } else {
+          this.countdownMinutes--;
+          this.countdownSeconds = 59;
+        }
+      } else {
+        this.countdownSeconds--;
+      }
+    }, 1000);
   }
 
 }
