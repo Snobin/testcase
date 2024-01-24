70146033d0e4925bc01a61af4a7a59d3a7e24da9
Mijohn T J <mijohn2000@gmail.com>
2024-01-24 17:13:46 +0530

    updated

--- frontend/testcasefront/src/app/pages/admin/users/users.component.ts
+++ frontend/testcasefront/src/app/pages/admin/users/users.component.ts
@@ -10,61 +10,21 @@ import Swal from 'sweetalert2';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  userdata: User=new User();
 
+  loginPage: boolean = true;
+  signupPage: boolean = false;
   constructor(private userservice:UserService,private snack:MatSnackBar,private renderer: Renderer2, private el: ElementRef) { }
-
-  private slider: HTMLElement;
-  private formSection: HTMLElement;
-  currentView: 'form' | 'file' = 'form';
-
-  toggleView(view: 'form' | 'file'): void {
-    this.currentView = view;
-  }
-
-
-  ngAfterViewInit() {
-    this.slider = this.el.nativeElement.querySelector('.slider');
-    this.formSection = this.el.nativeElement.querySelector('.form-section');
-  }
-
-  onSignupClick() {
-    this.renderer.addClass(this.slider, 'moveslider');
-    this.renderer.addClass(this.formSection, 'form-section-move');
-  }
-
-  onLoginClick() {
-    this.renderer.removeClass(this.slider, 'moveslider');
-    this.renderer.removeClass(this.formSection, 'form-section-move');
-  }
 
   ngOnInit(): void {
   }
-  formSubmit() {
-    console.log(this.userdata);
-    if(this.userdata.username==null||this.userdata.username==""){
-      this.snack.open("username is required ...",'',{
-        duration:3000,
-        verticalPosition:'top'
-      });
-      return;
-    }
-    this.userservice.addUser(this.userdata).subscribe(
-      (data)=>{
-        if (data.body == 'Successfully Inserted') {
-          // this.router.navigate([`./login`]);
-          Swal.fire("Success",'User is Registered','success')
-        } else {
-          Swal.fire('Error!', data.body,'error');
-        }
-      },
-      (error)=>{
-        console.log(error);
-        alert("error");
-      }
-    )
   
-
+  openLogin() {
+    this.loginPage = true;
+    this.signupPage = false;
   }
 
+  openSignup() {
+    this.loginPage = false;
+    this.signupPage = true;
+  }
 }
