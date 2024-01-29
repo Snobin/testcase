ee50aefd14b8d1728a2b496fb3be2a1fb68e7cbc
Parvathy7278 <parvathybabubabut@gmail.com>
2024-01-29 10:18:28 +0530

    huh

--- frontend/testcasefront/src/app/app.module.ts
+++ frontend/testcasefront/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
@@ -99,7 +98,7 @@ import { AdminupdateComponent } from './pages/admin/adminupdate/adminupdate.comp
 
     ServerErrorPageComponent,
     NotFoundComponent,
-    UserdetailComponent
+    UserdetailComponent,
 
     AdminupdateComponent,
 
