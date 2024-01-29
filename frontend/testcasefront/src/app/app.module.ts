a634f587f32cc944382888c1b7c1df9f37dbf6bc
Parvathy7278 <parvathybabubabut@gmail.com>
2024-01-29 10:33:00 +0530

    Revert "huh"
    This reverts commit ee50aefd14b8d1728a2b496fb3be2a1fb68e7cbc.

--- frontend/testcasefront/src/app/app.module.ts
+++ frontend/testcasefront/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
@@ -98,7 +99,7 @@ import { AdminupdateComponent } from './pages/admin/adminupdate/adminupdate.comp
 
     ServerErrorPageComponent,
     NotFoundComponent,
-    UserdetailComponent,
+    UserdetailComponent
 
     AdminupdateComponent,
 
