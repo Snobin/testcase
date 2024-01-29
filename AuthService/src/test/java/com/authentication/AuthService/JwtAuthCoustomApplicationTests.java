3639d7ab7abe763797b5fe5dff5f39ea5b77d8dc
Mijohn T J <mijohn2000@gmail.com>
2024-01-29 17:05:00 +0530

    Updated test name

--- AuthService/src/test/java/com/authentication/AuthService/JwtAuthCoustomApplicationTests.java
+++ AuthService/src/test/java/com/authentication/AuthService/JwtAuthCoustomApplicationTests.java
@@ -1,4 +1,4 @@
-package com.authentication.JwtAuthCoustom;
+package com.authentication.AuthService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
