3ec97ab582488fbbd93474c7a9e8addbb76d829b
Mijohn T J <mijohn2000@gmail.com>
2024-01-29 17:00:18 +0530

    updated jwtServices

---
+++ JwtAuthCoustom/src/main/java/com/authentication/AuthService/DTO/JwtResponseDTO.java
@@ -0,0 +1,26 @@
+package com.authentication.AuthService.DTO;
+
+public class JwtResponseDTO 
+{
+	private String token;
+    private String username;
+    private String role;
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
