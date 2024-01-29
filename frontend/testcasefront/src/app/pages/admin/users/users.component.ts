49009a4c0e6b3a29d726e5a65860b6c0224d77f1
Snobin <abrahamsnobin2001@gmail.com>
2024-01-29 15:15:59 +0530

    abc

--- frontend/testcasefront/src/app/pages/admin/users/users.component.ts
+++ frontend/testcasefront/src/app/pages/admin/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { error } from 'console';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/services/user.service';
 import Swal from 'sweetalert2';
@@ -88,14 +89,13 @@ ngOnInit(): void {
 
   uploadFile(){
     this.userservice.uploadExcelFile(this.fileContent).subscribe(
-      (response) => {
-        if (response.body == 'Users uploaded successfully') {
+      () => {
           this.router.navigate([`./userlist`]);
           Swal.fire("Success", 'Users uploaded successfully', 'success')
-        } else {
-          Swal.fire('Error!',response.body, 'error');
-        }
-      },
+        
+      },(error)=>{
+        Swal.fire('Error !!', 'error while adding New User', 'error');
+      }
     );
   }
 }
